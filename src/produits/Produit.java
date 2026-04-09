b6d0f799d04cd2a15ab44b873378accc9dab82d8
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2026-04-09 17:35:26 +0200

    steps 1-2-3

--- src/produits/Produit.java
+++ src/produits/Produit.java
@@ -1,5 +1,28 @@
 package produits;
 
-public class Produit {
+enum Unite {
+	GRAMME("g"),
+	KILOGRAMME("kg"),
+	LITRE("l"),
+	CENTILITRE("cl"),
+	MILLILITRE("ml"),
+	PAR_PIECE("piece");
+	
+	private final String text;
 
+    Unite(final String text) {
+        this.text = text;
+    }
+
+
+    @Override
+    public String toString() {
+        return text;
+    }
+}
+
+public abstract class Produit {
+	public abstract String getNom();
+	
+	public abstract String decrireProduit();
 }
